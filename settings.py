25acb80f17ceb55cc694d5fee9c0f6cb78eed708
Shou Chaofan <shouchaofan@shphschool.com>
2017-08-24 08:08:41 -0700

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-  
+
+#############settings#############
+NAME		= "1" #GitStar用户名
+PASSWORD	= "1" #GitStar密码
+GITNAME		= "1" #GitHub用户名
+GITPASSWORD	= "1" #GitHub密码
+#############settings#############
