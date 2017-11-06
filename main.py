df0d30b5ace51fe2aabe93a6eb03a55ba88224c0
dane.4 <weilaihui@126.com>
2017-11-06 18:18:26 +0800

    add update gitstar

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ class Gitstar():
 		self.PASSWORD 		= PASSWORD
 		self.GITNAME 		= GITNAME
 		self.GITPASSWORD 	= GITPASSWORD
+
+		self.cookie = None
 	def loginGitStar(self):
 		r=requests.post("http://gitstar.top:88/api/user/login",params={'username':self.NAME,'password':self.PASSWORD})
+		self.cookie = r.headers['Set-Cookie']
 		return r.headers['Set-Cookie']
 	def getGitStarList(self):
 		cookie=self.loginGitStar()
@@ -34,13 +37,22 @@ class Gitstar():
 	def star(self,url):
 		global AUTH
 		AUTH = HTTPBasicAuth(self.GITNAME, self.GITPASSWORD)
-		requests.put("https://api.github.com/user/starred/"+url
+		res = requests.put("https://api.github.com/user/starred/"+url
 			,headers={'Content-Length': '0'}
 			,auth=AUTH)
+	def update_gitstar(self):
+		url = "http://gitstar.top:88/update"
+		res = requests.get(url,headers={'Accept': 'application/json','Cookie':self.cookie})
+		print "update:" + str(res.status_code == 200)
 
 GS=Gitstar()
 urls = GS.getGitStarList()
+print "get total github repo:%d" % len(urls)
+i = 1
 for url in urls:
 	GS.star(url)
-	print "Stared! -->{}".format(url)
+	print "[%d]Stared! -->%s"%(i,url)
 	time.sleep(5.0)
+	i = i + 1
+if len(urls) > 0:
+	GS.update_gitstar()
